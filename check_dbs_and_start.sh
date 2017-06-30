d00d545561055531b5cd7be8a49df7f51bc82b97
Milot Mirdita <milot@mirdita.de>
2017-06-30 03:46:58 +0200

    Update db builder script

--- check_dbs_and_start.sh
+++ check_dbs_and_start.sh
@@ -16,36 +16,42 @@ function abspath() {
 export -f abspath
 
 function make_database_from_fasta() {
-	local INPUT="$(abspath $1)"
-	local OUTPUT="$(abspath $2)"
+    local INPUT="$(abspath $1)"
+    local OUTPUT="$(abspath $2)"
+    local MAXSENS="$3"
 
-	/usr/local/bin/mmseqs createdb "${INPUT}" "${OUTPUT}"
-	/usr/local/bin/mmseqs createindex "${OUTPUT}" --mask 2 --include-headers
+    1>&2 mmseqs createdb "${INPUT}" "${OUTPUT}"
+    1>&2 mmseqs createindex "${OUTPUT}" --mask 2 --include-headers --max-seq-len "${MAXSENS}"
 }
 export -f make_database_from_fasta
 
 function make_database_from_db() {
-	local INPUT="$(abspath $1)"
-	local OUTPUT="$(abspath $2)"
-	local MAXSENS="$3"
+    local INPUT="$(abspath $1)"
+    local OUTPUT="$(abspath $2)"
+    local MAXSENS="$3"
 
-	/usr/local/bin/mmseqs msa2profile "${INPUT}" "${OUTPUT}"
-	/usr/local/bin/mmseqs createindex "${OUTPUT}" -s "${MAXSENS}" --mask 2 --include-headers --target-profile
+    1>&2 mmseqs msa2profile "${INPUT}" "${OUTPUT}"
+
+    1>&2 echo "Finding longest entry ..."
+    local MAXSEQLEN="$(awk '$3 > max { max = $3 } END { print max+1 }' "${BASE}_seq.index")"
+    1>&2 mmseqs createindex "${OUTPUT}" -s "${MAXSENS}" --mask 2 --include-headers --target-profile --max-seq-len "${MAXSEQLEN}"
+
+    echo "${MAXSEQLEN}"
 }
 export -f make_database_from_db
 
 function parse_sense() {
-	local SENS=4
+    local SENS=4
 
-	while getopts ":s" opt; do
-		case $opt in
-			s)
-				SENS="$OPTARG"
-				;;
-		esac
-	done
+    while getopts "s:" opt; do
+        case $opt in
+            s)
+                SENS="$OPTARG"
+                ;;
+        esac
+    done
 
-	echo "$SENS"
+    echo "$SENS"
 }
 export -f parse_sense
 
@@ -56,52 +62,51 @@ function json2export() {
 export -f json2export
 
 function check_params() {
-	local PARAMS="$1"
-	local BASE="$(dirname "${PARAMS}")/$(basename "${PARAMS}" .params)"
+    local PARAMS="$1"
+    local BASE="$(dirname "${PARAMS}")/$(basename "${PARAMS}" .params)"
 
-	echo "Checking ${PARAMS}..."
-	eval "$(cat "$PARAMS" | json2export)";
+    echo "Checking ${PARAMS}..."
+    eval "$(cat "$PARAMS" | json2export)";
 
-	if [[ -s "${BASE}.fasta" ]]; then
-		echo "Building search database from ${BASE}.fasta ..."
-		make_database_from_fasta "${BASE}.fasta" "${BASE}"
+    if [[ -s "${BASE}.fasta" ]]; then
+        echo "Building search database from ${BASE}.fasta ..."
 
-		echo "Moving input fasta file to ${BASE}.fasta.bak ..."
-		mv "${BASE}.fasta" "${BASE}.fasta.bak"
-	elif [[ -s "${BASE}_msa" ]] && [[ -s "${BASE}_msa.index" ]]; then
+        echo "Finding longest entry ..."
+        PARAMS_MAXSEQLEN="$(awk '$3 > max { max = $3 } END { print max+1 }' "${BASE}.index")"
 
-		local SENSE=4
-		if [[ -n "${PARAMS_SEARCH}" ]]; then
-			SENSE="$(parse_sense ${PARAMS_SEARCH})"
-		fi
+        make_database_from_fasta "${BASE}.fasta" "${BASE}" "${PARAMS_MAXSEQLEN}"
 
-		echo "Building profile search database from ${BASE}_msa with SENS=${SENS} ..."
-		make_database_from_db "${BASE}_msa" "${BASE}" "${SENSE}"
+        echo "Moving input fasta file to ${BASE}.fasta.bak ..."
+        mv "${BASE}.fasta" "${BASE}.fasta.bak"
+    elif [[ -s "${BASE}_msa" ]] && [[ -s "${BASE}_msa.index" ]]; then
 
-		echo "Moving input database to ${BASE}_msa.bak ..."
-		mv "${BASE}_msa" "${BASE}_msa.bak"
-		mv "${BASE}_msa.index" "${BASE}_msa.index.bak"
-	fi
+        local SENSE=4
+        if [[ -n "${PARAMS_SEARCH}" ]]; then
+            SENSE="$(parse_sense ${PARAMS_SEARCH})"
+        fi
 
-	if [[ -z "${PARAMS_MAXSEQLEN}" ]]; then
-		echo "Finding longest entry ..."
-		MAXSEQLEN="$(awk '$3 > max { max = $3 } END { print max+1 }' "${BASE}.index")"
-		jq --arg maxSeqLen "${MAXSEQLEN}"  \
-			'. * {params : { maxseqlen: $maxSeqLen}}'  "${BASE}.params"  \
-				> "${BASE}.params.tmp"
-		mv -f "${BASE}.params.tmp" "${BASE}.params"
-	fi
+        echo "Building profile search database from ${BASE}_msa with SENS=${SENSE} ..."
+        PARAMS_MAXSEQLEN=$(make_database_from_db "${BASE}_msa" "${BASE}" "${SENSE}")
 
-	echo "Done. Starting Webserver..."
+        jq  '. * {params : { profile: 1 }}'  "${BASE}.params" > "${BASE}.params.tmp"
+        mv -f "${BASE}.params.tmp" "${BASE}.params"
+
+        echo "Moving input database to ${BASE}_msa.bak ..."
+        mv "${BASE}_msa" "${BASE}_msa.bak"
+        mv "${BASE}_msa.index" "${BASE}_msa.index.bak"
+    fi
+
+    if [[ -z "${PARAMS_MAXSEQLEN}" ]]; then
+        jq --arg maxSeqLen "${PARAMS_MAXSEQLEN}"  \
+            '. * {params : { maxseqlen: [ 32000, $maxSeqLen] | max }}'  "${BASE}.params"  \
+            > "${BASE}.params.tmp"
+        mv -f "${BASE}.params.tmp" "${BASE}.params"
+    fi
 }
 export -f check_params
 
+#find /big/search.mmseqs.com/databases -maxdepth 1 -type f -name '*.params' -exec bash -ex -c 'check_params "$0"' "{}" ';'
 find /opt/mmseqs-web/databases/ -maxdepth 1 -type f -name '*.params' -exec bash -ex -c 'check_params "$0"' "{}" ';'
 
-# wait_for() {
-#   echo Waiting for $1 to listen on $2...
-#   while ! nc -z $1 $2; do sleep 2; done
-# }
-
-# wait_for mmseqs-web-redis 6379
+echo "Done. Starting Webserver..."
 exec /usr/local/bin/mmseqs-web
