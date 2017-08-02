bc75a3db8d4c6ddddcec4e95247d0ab55b82c5be
Milot Mirdita <milot@mirdita.de>
2017-08-02 13:50:04 +0200

    Forgot the header entry

--- dbreader/dbreader.h
+++ dbreader/dbreader.h
@@ -23,5 +23,6 @@ const char* reader_get_data(void *reader, int64_t id);
 uint32_t reader_get_key(void *reader, int64_t id);
 int64_t reader_get_length(void *reader, int64_t id);
 int64_t reader_get_offset(void *reader, int64_t id);
+int64_t reader_get_size(void *r);
 
 #endif
