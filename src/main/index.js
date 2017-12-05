f7b333fd3465f26b8079bbb23001150ec6642444
Milot Mirdita <milot@mirdita.de>
2017-12-05 10:20:58 +0100

    Reformat

--- src/main/index.js
+++ src/main/index.js
@@ -1,65 +1,64 @@
-import { app, BrowserWindow, shell } from 'electron'
+import { app, BrowserWindow, shell } from 'electron';
 
-let mainWindow
 const winURL = process.env.NODE_ENV === 'development'
-  ? `http://localhost:9080`
-  : `file://${__dirname}/index.html`
+	? `http://localhost:9080` : `file://${__dirname}/index.html`;
 
-function createWindow () {
-  /**
-   * Initial window options
-   */
-  mainWindow = new BrowserWindow({
-    height: 563,
-    useContentSize: true,
-    width: 1000,
-    titleBarStyle: 'hiddenInset'
-  })
 
-  mainWindow.loadURL(winURL)
-
-  mainWindow.on('closed', () => {
-    mainWindow = null
-  })
-
-  mainWindow.webContents.on('new-window', (e, url) => {
-    if(url != mainWindow.webContents.getURL()) {
-      e.preventDefault()
-      shell.openExternal(url)
-    }
-  })
+let mainWindow;
+function createWindow() {
+	/**
+	* Initial window options
+	*/
+	mainWindow = new BrowserWindow({
+		height: 563,
+		useContentSize: true,
+		width: 1000,
+	});
+	
+	mainWindow.loadURL(winURL);
+	
+	mainWindow.on('closed', () => {
+		mainWindow = null;
+	});
+	
+	mainWindow.webContents.on('new-window', (e, url) => {
+		if (url != mainWindow.webContents.getURL()) {
+			e.preventDefault();
+			shell.openExternal(url);
+		}
+	});
 }
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
 
 app.on('activate', () => {
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
+	if (mainWindow === null) {
+		createWindow();
+	}
+});
 
 /**
- * Auto Updater
- *
- * Uncomment the following code below and install `electron-updater` to
- * support auto updating. Code Signing with a valid certificate is required.
- * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
- */
+* Auto Updater
+*
+* Uncomment the following code below and install `electron-updater` to
+* support auto updating. Code Signing with a valid certificate is required.
+* https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
+*/
 
 /*
 import { autoUpdater } from 'electron-updater'
 
 autoUpdater.on('update-downloaded', () => {
-  autoUpdater.quitAndInstall()
+	autoUpdater.quitAndInstall()
 })
 
 app.on('ready', () => {
-  if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
+	if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
 })
- */
+*/
