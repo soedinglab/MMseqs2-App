62fdfd8970da9e580aa8e5f5b80b9c6646ba3844
Milot Mirdita <milot@mirdita.de>
2018-09-11 16:24:55 +0200

    Fix database preparation scripts

--- docker-compose/examples/pdb70/setup.sh
+++ docker-compose/examples/pdb70/setup.sh
@@ -1,7 +1,10 @@
-#!/bin/bash
+#!/bin/bash -e
+SELF="$(dirname "$(readlink -f "$0")")"
 NAME=PDB70
-VERSION=28Jun17
-curl "http://wwwuser.gwdg.de/~compbiol/data/hhsuite/databases/hhsuite_dbs/pdb70_from_mmcif_${VERSION}.tgz" | tar -xzvf - pdb70_a3m.ffdata
-tr -d '\000' < pdb70_a3m.ffdata | awk -v outfile=pdb70_${VERSION}_msa -f pdb70.awk 
-rm -f pdb70_a3m.ffdata
-echo -e "{\n \"display\": {\n  \"name\": \"$NAME\",\n  \"version\": \"$VERSION\",\n  \"default\": true,\n  \"order\": 0\n },\n \"params\": {\n  \"search\": \"-s 5\"\n }\n}" > "pdb70_${VERSION}.params"
+VERSION=180820
+
+mkdir -p databases
+curl "http://wwwuser.gwdg.de/~compbiol/data/hhsuite/databases/hhsuite_dbs/pdb70_from_mmcif_${VERSION}.tar.gz" \
+    | tar -xOzf - pdb70_a3m.ffdata | tr -d '\000'| awk -v outfile="databases/pdb70_${VERSION}_msa" -f "${SELF}/pdb70.awk"
+
+echo -e "{\n \"display\": {\n  \"name\": \"$NAME\",\n  \"version\": \"$VERSION\",\n  \"default\": true,\n  \"order\": 0,\n  \"search\": \"-s 6\"\n }\n}\n" > "databases/pdb70_${VERSION}.params"
