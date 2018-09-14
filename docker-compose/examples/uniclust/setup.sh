65b03191bedf422efbe4eed36591821b164c6365
Milot Mirdita <milot@mirdita.de>
2018-09-14 13:39:55 +0200

    Uniclust download script works now

--- docker-compose/examples/uniclust/setup.sh
+++ docker-compose/examples/uniclust/setup.sh
@@ -5,5 +5,6 @@ SEQID="30"
 KIND="seed"
 
 mkdir -p databases
-(cd databases; wget "http://gwdu111.gwdg.de/~compbiol/uniclust/${VERSION}/uniclust30_${VERSION}.tar.gz" | tar -xzvf - "uniclust30_${VERSION}_${KIND}.fasta" )
-echo -e "{\n \"display\": {\n  \"name\": \"$NAME\",\n  \"version\": \"$VERSION\",\n  \"default\": true,\n  \"order\": 0\n },\n \"params\": {\n  \"search\": \"-s 5\"\n }\n}" > "databases/uniclust30_${VERSION}_${KIND}.params"
+(cd databases; curl "http://gwdu111.gwdg.de/~compbiol/uniclust/${VERSION}/uniclust30_${VERSION}.tar.gz" | tar --strip-components=1 -xzvf - "uniclust30_${VERSION}/uniclust30_${VERSION}_${KIND}.fasta"; rm -f "uniclust30_${VERSION}.tar.gz")
+echo -e "{\n \"display\": {\n  \"name\": \"$NAME\",\n  \"version\": \"$VERSION\",\n  \"default\": true,\n  \"order\": 0,\n  \"search\": \"-s 5\"\n }\n}" > "databases/uniclust30_${VERSION}_${KIND}.params"
+
