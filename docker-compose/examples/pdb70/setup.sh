51cc1f179c61e0a1c981feda3fffff3418205206
Milot Mirdita <milot@mirdita.de>
2021-04-16 21:09:11 +0200

    Fix pdb70 download script

--- docker-compose/examples/pdb70/setup.sh
+++ docker-compose/examples/pdb70/setup.sh
@@ -6,5 +6,6 @@ VERSION=latest
 mkdir -p databases
 curl "http://wwwuser.gwdg.de/~compbiol/data/hhsuite/databases/hhsuite_dbs/pdb70_from_mmcif_${VERSION}.tar.gz" \
     | tar -xOzf - pdb70_a3m.ffdata | tr -d '\000'| awk -v outfile="databases/pdb70_${VERSION}_msa" -f "${SELF}/pdb70.awk"
+awk 'BEGIN { printf("%c%c%c%c",11,0,0,0); exit; }' > "databases/pdb70_${VERSION}_msa.dbtype"
 
 echo -e "{\n \"display\": {\n  \"name\": \"$NAME\",\n  \"version\": \"$VERSION\",\n  \"default\": true,\n  \"order\": 0,\n  \"index\": \"-s 6\",\n  \"search\": \"-s 6\"\n }\n}\n" > "databases/pdb70_${VERSION}.params"
