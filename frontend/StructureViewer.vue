9e6b7503361c019876e404f1f2fd9e96fc3c69dc
Cameron Gilchrist <clmgilchrist@gmail.com>
2022-04-12 17:07:53 +0900

    Guide superposition with foldseek alignment

--- frontend/StructureViewer.vue
+++ frontend/StructureViewer.vue
@@ -33,7 +33,7 @@
 </template>
 
 <script>
-import { Shape, Stage, superpose } from 'ngl';
+import { Shape, Stage, Superposition } from 'ngl';
 import Panel from './Panel.vue';
 
 // Create NGL arrows from array of ([X, Y, Z], [X, Y, Z]) pairs
@@ -105,26 +105,33 @@ export default {
         'targetMap': { type: Map, default: null },
     },
     methods: {
-        // Parse two strings and pair corresponding XYZ coordinates from two arrays if both
-        // characters are not '-'
-        parseAlns(aln1, aln2, qStruc, tStruc) {
-            const matches = new Array()
-            let qAtoms = qStruc.atomStore
-            let tAtoms = tStruc.atomStore
-            for (let i = 0; i < aln1.length; i++) {
-                if (aln1[i] !== '-' && aln2[i] !== '-') {
-                    let qPos = this.queryMap.get(i) - 1
-                    let tPos = this.targetMap.get(i) - 1
-                    // Don't grab any coordinates outside of the current selection
-                    // NGL selections are 1-indexed, so subtract 1 from the selection start
-                    if (this.selection && !(tPos >= this.selection[0] - 1 && tPos < this.selection[1]))
-                        continue
-                    let qXYZ = [qAtoms.x[qPos], qAtoms.y[qPos], qAtoms.z[qPos]]
-                    let tXYZ = [tAtoms.x[tPos], tAtoms.y[tPos], tAtoms.z[tPos]]
-                    matches.push([qXYZ, tXYZ])
-                }
+        superposeStructures(qStruc, tStruc) {
+            let atoms1 = this.qMatches.flatMap(match => match.xyz())
+            let atoms2 = this.tMatches.flatMap(match => match.xyz())
+            var matrix = new Superposition(new Float32Array(atoms2), new Float32Array(atoms1))
+            matrix.transform(tStruc)
+            qStruc.refreshPosition()
+        },
+        // Parses two alignment strings, and saves matching residues
+        // Each match contains the index of the residue in the structure and a callback
+        // function to retrieve the residue's CA XYZ coordinates to allow retrieval
+        // before and after superposition (with updated coords)
+        saveMatchingResidues(aln1, aln2, str1, str2) {
+            if (aln1.length !== aln2.length) return
+            this.qMatches = []
+            this.tMatches = []
+            const xyz = (map, index, structure) => {
+                var rp = structure.getResidueProxy()
+                rp.index = map.get(index) - 1
+                var ap = structure.getAtomProxy()
+                ap.index = rp.getAtomIndexByName('CA')
+                return [ap.x, ap.y, ap.z]
             }
-            return matches
+            for (let i = 0; i < aln1.length; i++) {
+                if (aln1[i] === '-' || aln2[i] === '-') continue
+                this.qMatches.push({ index: this.queryMap.get(i) - 1, xyz: () => xyz(this.queryMap, i, str1) })
+                this.tMatches.push({ index: this.targetMap.get(i) - 1, xyz: () => xyz(this.targetMap, i, str2) })
+            }
         },
         handleResize() {
             if (!this.$refs.viewport) return
@@ -158,12 +165,14 @@ export default {
         renderArrows() {
             if (!this.stage) return
             if (this.arrowShape) this.arrowShape.dispose()
-            let matches = this.parseAlns(
-                this.alignment.qAln,
-                this.alignment.dbAln,
-                this.queryRepr.parent.structure,
-                this.targetRepr.parent.structure,
-            )
+            let matches = new Array()
+            for (let i = 0; i < this.tMatches.length; i++) {
+                let qMatch = this.qMatches[i]
+                let tMatch = this.tMatches[i]
+                if (this.selection && !(tMatch.index >= this.selection[0] - 1 && tMatch.index < this.selection[1]))
+                    continue
+                matches.push([qMatch.xyz(), tMatch.xyz()])
+            }
             this.arrowShape = this.stage.addComponentFromObject(createArrows(matches))
             this.arrowShape.addRepresentation('buffer')
             this.arrowShape.setVisibility(this.showArrows)
@@ -187,13 +196,13 @@ export default {
         const bgColor = this.$vuetify.theme.dark ? this.bgColourDark : this.bgColourLight;
         if (typeof(this.alignment.qCa) == "undefined" || typeof(this.alignment.tCa) == "undefined")
             return;
-        console.log(this.alignment)
         this.stage = new Stage(this.$refs.viewport, { backgroundColor: bgColor, ambientIntensity: 0.40 })
         Promise.all([
             this.stage.loadFile(mockPDB(this.alignment.qCa), {ext: 'pdb', firstModelOnly: true}),
             this.stage.loadFile(mockPDB(this.alignment.tCa), {ext: 'pdb', firstModelOnly: true})
         ]).then(([query, target]) => {
-            superpose(target.structure, query.structure, {align: true})
+            this.saveMatchingResidues(this.alignment.qAln, this.alignment.dbAln, query.structure, target.structure)
+            this.superposeStructures(query.structure, target.structure)
             this.queryRepr = query.addRepresentation(this.qRepr, {color: this.qColour})
             this.targetRepr = target.addRepresentation(
                 this.tRepr,
