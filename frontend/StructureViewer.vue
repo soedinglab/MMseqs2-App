2b2fd77df39e410b07dd9018d30cfe02e8fbf55a
Milot Mirdita <milot@mirdita.de>
2023-02-02 02:25:03 +0900

    Add explanation for navigating the structure viewer

--- frontend/StructureViewer.vue
+++ frontend/StructureViewer.vue
@@ -1,6 +1,47 @@
 <template>
     <div class="structure-panel" v-if="'tCa' in alignment">
         <div class="structure-wrapper" ref="structurepanel">
+            <v-tooltip open-delay="300" bottom attach=".structure-wrapper" background-color="transparent">
+                <template v-slot:activator="{ on }">
+                    <v-icon :light="isFullscreen" v-on="on" style="position: absolute; z-index: 999; right:0">{{ $MDI.HelpCircleOutline }}</v-icon>
+                </template>
+                <span>
+                    <dl style="text-align: center;">
+                        <dt>
+<svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2" viewBox="0 0 32 32">
+<title>Left click</title>
+<path d="M25.6 5.8a5 5 0 0 0-5-4.8h-9.1a5 5 0 0 0-5.1 4.8v20.4a5 5 0 0 0 5 4.8h9.1a5 5 0 0 0 5.1-4.8V5.8Zm-1 9.5v10.9a4 4 0 0 1-4 3.8h-9.1a4 4 0 0 1-4-3.8V15.3h17ZM15.5 2v12.3h-8V5.8a4 4 0 0 1 4-3.8h4Zm1 0h4a4 4 0 0 1 4 3.8v8.5h-8V2Z"/>
+<path id="left" d="M15.5 2v12.3h-8V5.8a4 4 0 0 1 4-3.8h4Z" style="fill:red"/>
+<path id="middle-inactive" d="M14.6 4h2.8v8h-2.8z"/>
+</svg>
+                        </dt>
+                        <dd>
+                            Rotate
+                        </dd>
+                        <dt>
+<svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2" viewBox="0 0 32 32">
+<title>Right click</title>
+<path d="M25.6 5.8a5 5 0 0 0-5-4.8h-9.1a5 5 0 0 0-5.1 4.8v20.4a5 5 0 0 0 5 4.8h9.1a5 5 0 0 0 5.1-4.8V5.8Zm-1 9.5v10.9a4 4 0 0 1-4 3.8h-9.1a4 4 0 0 1-4-3.8V15.3h17ZM15.5 2v12.3h-8V5.8a4 4 0 0 1 4-3.8h4Zm1 0h4a4 4 0 0 1 4 3.8v8.5h-8V2Z"/>
+<path id="right" d="M16.5 2h4a4 4 0 0 1 4 3.8v8.5h-8V2Z" style="fill:red"/>
+<path id="middle-inactive" d="M14.6 4h2.8v8h-2.8z"/>
+</svg>
+                        </dt>
+                        <dd>
+                            Pan
+                        </dd>
+                        <dt>
+<svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2" viewBox="0 0 32 32">
+<title>Scroll wheel</title>
+<path d="M25.6 5.8a5 5 0 0 0-5-4.8h-9.1a5 5 0 0 0-5.1 4.8v20.4a5 5 0 0 0 5 4.8h9.1a5 5 0 0 0 5.1-4.8V5.8Zm-1 9.5v10.9a4 4 0 0 1-4 3.8h-9.1a4 4 0 0 1-4-3.8V15.3h17ZM15.5 2v12.3h-8V5.8a4 4 0 0 1 4-3.8h4Zm1 0h4a4 4 0 0 1 4 3.8v8.5h-8V2Z"/>
+<path id="middle-active" d="M14.6 4h2.8v8h-2.8z" style="fill:red"/>
+</svg>
+                        </dt>
+                        <dd>
+                            Zoom
+                        </dd>
+                    </dl>
+                </span>
+            </v-tooltip>
             <div v-if="tmAlignResults" class="tmscore-panel" v-bind="tmPanelBindings">
                 TM-Score: {{ tmAlignResults.tmScore }}
                 <br>
@@ -482,6 +523,10 @@ END
 .structure-wrapper {
     width: 400px;
     height: 300px;
+}
+
+.theme--dark .structure-wrapper .v-tooltip__content {
+    background: rgba(97, 97, 97, 0.3);
 }
 /* @media only screen and (max-width: 600px) {
     .structure-wrapper {
