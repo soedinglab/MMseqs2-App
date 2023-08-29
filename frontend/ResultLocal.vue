d8738df4dcd06b065380a601a3c1459d33d1e617
Cameron Gilchrist <clmgilchrist@gmail.com>
2023-08-29 15:25:49 +0900

    add header with JSON data file loader

--- frontend/ResultLocal.vue
+++ frontend/ResultLocal.vue
@@ -1,11 +1,32 @@
 <template>
     <div>
+        <v-app-bar app :height="'48px'" fixed clipped-left :class="['ml-0', 'pl-3', null]">
+            <v-app-bar-title>{{ $STRINGS.APP_NAME }} Search Results</v-app-bar-title>
+            <v-spacer />
+            <v-file-input
+                id="uploadData"
+                class="shrink"
+                type="file"
+                accept="application/json"
+                placeholder="Load JSON data file"
+                style="position: relative; top: 30%;"
+                @change="uploadData" 
+                single-line
+                outlined
+                filled
+                flat
+                dense
+            />
+            <v-toolbar-items v-once class="hidden-sm-and-down">
+                <v-btn text rel="external noopener" target="_blank"
+                       v-for="i in ($STRINGS.NAV_URL_COUNT - 0)" :key="i" :href="$STRINGS['NAV_URL_' + i]">{{ $STRINGS["NAV_TITLE_" + i]}}</v-btn>
+            </v-toolbar-items>
+        </v-app-bar>
         <v-tabs center-active grow style="margin-bottom: 1em" show-arrows>
             <v-tab v-for="(entry, index) in hits" :key="entry.query.header" @click="changeResult(index)">
                 {{ entry.query.header }} ({{ entry.results[0].alignments ? entry.results[0].alignments.length : 0 }})
             </v-tab>
         </v-tabs>
-        <input id="uploadData" type="file" @change="uploadData" />
         <ResultView
             :key="currentIndex"
             :ticket="ticket"
@@ -14,18 +35,33 @@
             :hits="currentResult"
             :selectedDatabases="selectedDatabases"
             :tableMode="tableMode"
-        />       
+        />
+        <v-container grid-list-md fluid pa-2>
+            <v-layout wrap>
+                <v-flex xs12>
+                    <v-card rounded="0">
+                    <v-card-title primary-title class="pb-0 mb-0">
+                        <div class="text-h5 mb-0">Reference</div>
+                    </v-card-title>
+                    <v-card-title primary-title class="pt-0 mt-0">
+                        <p class="text-subtitle-2 mb-0" v-html="$STRINGS.CITATION"></p>
+                    </v-card-title>
+                    </v-card>
+                </v-flex>
+            </v-layout>
+        </v-container>
     </div>
 </template>
 
 <script>
 import ResultMixin from './ResultMixin.vue';
 import ResultView from './ResultView.vue';
+import Navigation from './Navigation.vue';
 
 export default {
     name: 'result',
     mixins: [ResultMixin],
-    components: { ResultView },
+    components: { ResultView, Navigation },
     data() {
         return {
             currentIndex: 0
@@ -56,17 +92,16 @@ export default {
             this.currentIndex = newRes;
             this.setColorScheme();
         },
-        uploadData(event) {
-            const file = event.target.files[0];
+        uploadData(file) {
+            if (!file) {
+                return;
+            }
             let fr = new FileReader();
             fr.addEventListener(
                 "load",
                 (e) => {
-                    let str = e.target.result;
-                    this.hits = [];
-                    for (const hit of JSON.parse(str)) {
-                        this.hits.push(this.parseResults(hit));
-                    }
+                    let data = JSON.parse(e.target.result);
+                    this.fetchData(data);
                 }
             );
             fr.readAsText(file)
@@ -89,3 +124,9 @@ export default {
     }
 };
 </script>
+
+<style scoped>
+::v-deep .v-app-bar-title__content {
+    text-overflow: revert !important;
+}
+</style>
