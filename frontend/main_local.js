3dba49710515a794a77397f331f2d03308a7515a
Cameron Gilchrist <clmgilchrist@gmail.com>
2023-09-25 11:45:14 +0900

    move MDI icons to separate library

--- frontend/main_local.js
+++ frontend/main_local.js
@@ -1,42 +1,10 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 import Portal from './lib/vue-simple-portal';
-
-import {
-    mdiHistory,
-    mdiChevronLeft,
-    mdiChevronRight,
-    mdiClockOutline,
-    mdiAlertCircleOutline,
-    mdiHelpCircleOutline,
-    mdiMagnify,
-    mdiTune,
-    mdiDns,
-    mdiReorderHorizontal,
-    mdiDelete,
-    mdiFileDownloadOutline,
-    mdiCloudDownloadOutline,
-    mdiFormatListBulleted,
-    mdiLabel,
-    mdiLabelOutline,
-    mdiNotificationClearAll,
-    mdiProgressWrench,
-    mdiRestore,
-    mdiFullscreen,
-    mdiArrowRightCircle,
-    mdiArrowRightCircleOutline,
-    mdiCircle,
-    mdiCircleHalf,
-    mdiPlusBox,
-    mdiMinusBox,
-} from '@mdi/js'
+import MDI from './MDI.js';
 
 Vue.use(Vuetify);
 Vue.use(Portal);
-
-import { Shape, Stage, Selection, download, ColormakerRegistry, PdbWriter } from 'ngl';
-import { pulchra } from 'pulchra-wasm';
-import { tmalign, parse, parseMatrix } from 'tmalign-wasm';
 
 import AppLocal from './AppLocal.vue';
 const appStrings = {
@@ -62,34 +30,7 @@ Vue.use({
         Vue.prototype.$STRINGS = appStrings[__APP__];
         Vue.prototype.$ELECTRON = __ELECTRON__;
         Vue.prototype.$LOCAL = __LOCAL__;
-        Vue.prototype.$MDI = {
-            History: mdiHistory,
-            ChevronLeft: mdiChevronLeft,
-            ChevronRight: mdiChevronRight,
-            ClockOutline: mdiClockOutline,
-            AlertCircleOutline: mdiAlertCircleOutline,
-            HelpCircleOutline: mdiHelpCircleOutline,
-            Magnify: mdiMagnify,
-            Tune: mdiTune,
-            Dns: mdiDns,
-            ReorderHorizontal: mdiReorderHorizontal,
-            Delete: mdiDelete,
-            FileDownloadOutline: mdiFileDownloadOutline,
-            CloudDownloadOutline: mdiCloudDownloadOutline,
-            FormatListBulleted: mdiFormatListBulleted,
-            Label: mdiLabel,
-            LabelOutline: mdiLabelOutline,
-            NotificationClearAll: mdiNotificationClearAll,
-            ProgressWrench: mdiProgressWrench,
-            Restore: mdiRestore,
-            Fullscreen: mdiFullscreen,
-            ArrowRightCircle: mdiArrowRightCircle,
-            ArrowRightCircleOutline: mdiArrowRightCircleOutline,
-            Circle: mdiCircle,
-            CircleHalf: mdiCircleHalf,
-            PlusBox: mdiPlusBox,
-            MinusBox: mdiMinusBox,
-        };
+        Vue.prototype.$MDI = MDI;
         Vue.prototype.__OS__ = { arch: 'web', platform: 'web' };
         Vue.prototype.mmseqsVersion = "web";
         Vue.prototype.saveResult = () => {};
