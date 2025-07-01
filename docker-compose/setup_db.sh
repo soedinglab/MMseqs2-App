9c7e85fe118b357b6aa3cc9e8fcf4d0a0863edf7
Milot Mirdita <milot@mirdita.de>
2025-07-01 21:27:38 +0900

    Allow setup db script to be used outside docker

--- docker-compose/setup_db.sh
+++ docker-compose/setup_db.sh
@@ -4,33 +4,35 @@ if [ "$#" = "0" ]; then
     exit 0;
 fi
 
+DBPATH="${DBPATH:-"/opt/mmseqs-web/databases"}"
+
 for DB in "${@}"; do
 
 SAFE=$(echo "${DB}" | tr -cd '[a-zA-Z0-9]._-')
-if [ ! -e "/opt/mmseqs-web/databases/${SAFE}.dbtype" ]; then
-    "${APP}" databases "${DB}" "/opt/mmseqs-web/databases/${SAFE}" "/opt/mmseqs-web/databases/tmp_${SAFE}" || continue
+if [ ! -e "$DBPATH/${SAFE}.dbtype" ]; then
+    "${APP}" databases "${DB}" "$DBPATH/${SAFE}" "$DBPATH/tmp_${SAFE}" || continue
 fi
 
-if [ ! -e "/opt/mmseqs-web/databases/${SAFE}.idx.dbtype" ]; then
-    "${APP}" createindex "/opt/mmseqs-web/databases/${SAFE}" "/opt/mmseqs-web/databases/tmp_${SAFE}" --split 1
+if [ ! -e "$DBPATH/${SAFE}.idx.dbtype" ]; then
+    "${APP}" createindex "$DBPATH/${SAFE}" "$DBPATH/tmp_${SAFE}" --split 1
 fi
 
-rm -rf -- "/opt/mmseqs-web/databases/tmp_${SAFE}"
+rm -rf -- "$DBPATH/tmp_${SAFE}"
 
-VERSION=$(cat "/opt/mmseqs-web/databases/${SAFE}.version" | head -n1 | awk '{ print $1; }')
+VERSION=$(cat "$DBPATH/${SAFE}.version" | head -n1 | awk '{ print $1; }')
 TAXONOMY=$("${APP}" databases --tsv | awk -v db="${DB}" '$1 == db { printf("%s", $3); f=1; exit; } END { if (f==0) printf("false"); }')
 
 if [ "${TAXONOMY}" = "true" ]; then
-    if [ -e "/opt/mmseqs-web/databases/${SAFE}_mapping" ]; then
-        TAXHEADER=$(od -An -N 4 -w6 -t x1 "/opt/mmseqs-web/databases/${SAFE}_mapping" | tr -d ' ')
+    if [ -e "$DBPATH/${SAFE}_mapping" ]; then
+        TAXHEADER=$(od -An -N 4 -w6 -t x1 "$DBPATH/${SAFE}_mapping" | tr -d ' ')
         # magic header TAXM in hex
         if [ "${TAXHEADER}" != "1300170c" ]; then
-            "${APP}" createbintaxmapping "/opt/mmseqs-web/databases/${SAFE}_mapping" "/opt/mmseqs-web/databases/${SAFE}_mapping.bin"
-            mv -f "/opt/mmseqs-web/databases/${SAFE}_mapping.bin" "/opt/mmseqs-web/databases/${SAFE}_mapping"
+            "${APP}" createbintaxmapping "$DBPATH/${SAFE}_mapping" "$DBPATH/${SAFE}_mapping.bin"
+            mv -f "$DBPATH/${SAFE}_mapping.bin" "$DBPATH/${SAFE}_mapping"
         fi
     fi
 fi
 
-echo "{\n  \"status\": \"COMPLETE\",\n  \"name\": \"${DB}\",\n  \"version\": \"${VERSION}\",\n  \"path\": \"${SAFE}\",\n  \"taxonomy\": ${TAXONOMY},\n  \"default\": true,\n  \"order\": 0,\n  \"index\": \"\",\n  \"search\": \"\"\n}" > "/opt/mmseqs-web/databases/${SAFE}.params"
+echo "{\n  \"status\": \"COMPLETE\",\n  \"name\": \"${DB}\",\n  \"version\": \"${VERSION}\",\n  \"path\": \"${SAFE}\",\n  \"taxonomy\": ${TAXONOMY},\n  \"default\": true,\n  \"order\": 0,\n  \"index\": \"\",\n  \"search\": \"\"\n}" > "$DBPATH/${SAFE}.params"
 
 done
