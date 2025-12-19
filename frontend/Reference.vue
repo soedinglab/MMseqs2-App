8299bafef20aaa9da53d47c58ef8e0fb2913992c
Milot Mirdita <milot@mirdita.de>
2025-12-20 00:56:03 +0900

    Highlight FOSS

--- frontend/Reference.vue
+++ frontend/Reference.vue
@@ -1,5 +1,5 @@
 <template>
-        <v-layout wrap>
+    <v-layout wrap>
         <v-flex xs12>
             <v-card rounded="0">
             <v-card-title primary-title class="pb-0 mb-0">
@@ -9,6 +9,9 @@
                 <p class="text-subtitle-2 mb-0" v-html="reference"></p>
             </v-card-title>
             </v-card>
+            <div style="text-align: center; color: grey; margin-top: 0.9rem; font-size: 0.9rem;">
+                The {{ $STRINGS.APP_NAME }} Server is proudly <a :href="$STRINGS.NAV_URL_1" style="text-decoration: none; color: darkgray">free and open-source software</a>.
+            </div>
         </v-flex>
     </v-layout>
 </template>
